5ada00642168baef6aa53e33443bc825fa985bec
Kumaru2 <60379752+Kumaru2@users.noreply.github.com>
2020-05-14 12:58:26 +0200

    nav
    Color and stuff

--- nav.js
+++ nav.js
@@ -9,14 +9,7 @@ const navSlide = () => {
         //toggle nav
         nav.classList.toggle("nav-active");
 
-        //animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation){
-                link.style.animation = "";
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index/7+1}s`;
-            }
-        });
+        
         //Burger animation
         burger.classList.toggle("toggle");
 
