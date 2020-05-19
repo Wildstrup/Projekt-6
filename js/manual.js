bb0ba5a8ccb6c4b5241987905b82f6efa0deb5e0
Wildstrup <60379913+Wildstrup@users.noreply.github.com>
2020-05-20 01:32:38 +0200

    Manual
    Der er kommet Java, men virker ikke helt. Og Css dertil

---
+++ js/manual.js
@@ -0,0 +1,32 @@
+const slider = document.querySelector('.slider');
+
+const leftArrow = document.querySelector('.left');
+const rightArrow = document.querySelector('.right');
+const indicatorParents = document.querySelector('.controls ul');
+
+var sectionIndex = 0;
+
+function setIndex() {
+  document.querySelector('.controls .selected').classList.remove('.selected');
+  slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
+}
+
+document.querySelectorAll('.controls li').forEach(function(indicator, ind) {
+  indicator.addEventListener('click', function() {
+    sectionIndex = ind;
+    indicator.classList.add('selected');
+    setIndex(sectionIndex);
+  });
+});
+
+leftArrow.addEventListener('click', function() {
+  sectionIndex = (section > 0) ? sectionIndex - 1 : 0;
+  setIndex();
+  indicatorParents.children[sectionIndex].classList.add('selected');
+});
+
+rightArrow.addEventListener('click', function() {
+  sectionIndex = (section < 3) ? sectionIndex + 1 : 3;
+  setIndex();
+  indicatorParents.children[sectionIndex].classList.add('selected');
+});
